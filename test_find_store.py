53afb42f194aa55c81ffa151342a176ec9f684d0
gurdipsingh <gurdipsingh1990@gmail.com>
2020-09-29 22:36:21 -0700

    saving latet

---
+++ test_find_store.py
@@ -0,0 +1,16 @@
+
+import unittest
+from docopt import docopt
+from find_store import find_by_zip
+
+
+class FindByZipTest(unittest.TestCase):
+    def test_set_up(self):
+        """TEST"""
+        # find = find_by_zip('93922', 'mi', 'json')
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+    # from find_store import find_by_zip
